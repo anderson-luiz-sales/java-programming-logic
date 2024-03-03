820134fe0e37a077dfc39802400caebfffbb6d27
Anderson Luiz Sales <CIANDT\andersonls@lnb035888cps.ciandt.global>
2024-03-03 18:44:45 -0300

    Criação dos exercicio da estrutura condicional simples

---
+++ src/condicional/simples/exercicio01.java
@@ -0,0 +1,20 @@
+package condicional.simples;
+
+import java.util.Scanner;
+
+public class exercicio01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite sua idade:");
+        int idade = sc.nextInt();
+
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade.");
+        } else {
+            System.out.println("Você é menor de idade.");
+        }
+
+        sc.close();
+    }
+}
---
+++ src/condicional/simples/exercicio08.java
@@ -0,0 +1,24 @@
+package condicional.simples;
+
+import java.util.Scanner;
+
+public class exercicio08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Idade do candidato:");
+        int idade = scanner.nextInt();
+
+        System.out.println("Condição física do candidato? (s/n):");
+        char condicaoFisica = scanner.next().charAt(0);
+
+        if (idade < 70 && condicaoFisica == 's') {
+            System.out.println("Inscrever-se para a viagem espacial.");
+        }
+        if (!(idade < 70 && condicaoFisica == 's')) {
+            System.out.println("Não se inscrever para a viagem espacial.");
+        }
+
+        scanner.close();
+    }
+}
