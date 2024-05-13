37ac71453c89f531d3c20ee82f5b918219287a7d
anderson-luiz-sales <anderson.luiz.sales@hotmail.com>
2024-05-13 19:29:13 -0300

    criação do exercico01 da estrutura while parte II

--- src/repetitiva/while_/parte_II/Exercicio01.java
+++ src/repetitiva/while_/parte_II/Exercicio01.java
@@ -4,7 +4,39 @@ import java.util.Scanner;
 
 public class Exercicio01 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
+        int totalNotas = 0;
+        double somaNotas = 0;
+        System.out.println("Digite as notas do aluno (0 a 10). Insira um valor fora desse intervalo para encerrar: ");
+
+        while (true) {
+            System.out.print("Digite a nota do aluno: ");
+            double nota = sc.nextDouble();
+
+            if (nota < 0 || nota > 10) {
+                System.out.println("Nota inválida! Por favor, insira uma nota entre 0 e 10.");
+                continue; // Solicita novamente a nota
+            }
+
+            totalNotas++;
+            somaNotas = somaNotas + nota;
+
+            System.out.print("Deseja inserir outra nota? (S/N): ");
+            String continuar = sc.next();
+
+            if (!continuar.equals("S")) {
+                break;
+            }
+        }
+
+        if (totalNotas == 0) {
+            System.out.println("Nenhuma nota foi inserida.");
+        } else {
+            double media = somaNotas / totalNotas;
+            System.out.println("A média das notas é: " + media);
+        }
+
+        sc.close();
     }
 }
